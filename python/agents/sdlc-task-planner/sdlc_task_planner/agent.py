8b32e13eaf3e414ad8d0ff353997e69fd937d344
gagata <gagata@google.com>
2026-04-16 09:31:35 +0000

    Address the unsupported artifact type in GE

--- python/agents/sdlc-task-planner/sdlc_task_planner/agent.py
+++ python/agents/sdlc-task-planner/sdlc_task_planner/agent.py
@@ -1,13 +1,16 @@
 from google.adk.agents import LlmAgent
+from google.adk.agents.callback_context import CallbackContext
+from google.genai import types
 
 from .config import config
 from .prompt import get_prompt
 from .tools.artifact_tools import save_artifact
+
 
 root_agent = LlmAgent(
     name="sdlc_task_planner_agent",
     model=config.default_llm,
     description="Agent that manages task creation and outputs a detailed MR plan based on technical design.",
     instruction=get_prompt(),
-    tools=[save_artifact],
+    tools=[],
 )
